e6768ffe8ae0719454e932d69f571ac528997c4e
Oby <obyli214@gmail.com>
2022-09-05 05:29:03 -0700

    added js logic and finished app

---
+++ script.js
@@ -0,0 +1,30 @@
+const count = document.querySelector('.count')
+const buttons = document.querySelector('.buttons')
+// const add = document.querySelector('.add')
+// const resetCount = document.querySelector('.reset')
+// const sub = document.querySelector('.subtract')
+
+buttons.addEventListener('click', (evt) => {
+    if (evt.target.classList.contains('add')) {
+        count.innerHTML++
+    }
+
+    if (evt.target.classList.contains('subtract')) {
+        count.innerHTML--
+    }
+
+    if (evt.target.classList.contains('reset')) {
+        count.innerHTML = 0
+    }
+    count.innerHTML < 0 ? count.style.color = "red" : count.style.color = "white"
+})
+
+// add.addEventListener('click', () => {
+//     count.innerHTML++
+// })
+// sub.addEventListener('click', () => {
+//     count.innerHTML--
+// })
+// resetCount.addEventListener('click', () => {
+//     count.innerHTML = 0
+// })

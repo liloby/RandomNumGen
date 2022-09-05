718a13cfa10ec7fccb821ad725862dd23df2cec2
Oby <obyli214@gmail.com>
2022-09-05 05:39:11 -0700

    polish UI and UX

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-const count = document.querySelector('.count')
-const buttons = document.querySelector('.buttons')
+const count = document.querySelector(".count");
+const buttons = document.querySelector(".buttons");
 // const add = document.querySelector('.add')
 // const resetCount = document.querySelector('.reset')
 // const sub = document.querySelector('.subtract')
 
-buttons.addEventListener('click', (evt) => {
-    if (evt.target.classList.contains('add')) {
-        count.innerHTML++
-    }
+buttons.addEventListener("click", (evt) => {
+  if (evt.target.classList.contains("add")) {
+    count.innerHTML++;
+  }
 
-    if (evt.target.classList.contains('subtract')) {
-        count.innerHTML--
-    }
+  if (evt.target.classList.contains("subtract")) {
+    count.innerHTML--;
+  }
 
-    if (evt.target.classList.contains('reset')) {
-        count.innerHTML = 0
-    }
-    count.innerHTML < 0 ? count.style.color = "red" : count.style.color = "white"
-})
+  if (evt.target.classList.contains("reset")) {
+    count.innerHTML = 0;
+  }
+  count.innerHTML < 0
+    ? (count.style.color = "orangered")
+    : count.innerHTML > 0
+    ? (count.style.color = "yellow")
+    : (count.style.color = "white");
+});
 
 // add.addEventListener('click', () => {
 //     count.innerHTML++
